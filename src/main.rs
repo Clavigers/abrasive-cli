675069689d689d14d3996a0d211a6bb8e37b75f5
wmurra <william.matthew.murray@gmail.com>
2026-04-03 11:12:08 -0700

    wasteland: hello world cli

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "abrasive", version, about = "The Abrasive CLI")]
+struct Cli {}
+
+fn main() {
+    let _cli = Cli::parse();
+    println!("Hello, world!");
+}
